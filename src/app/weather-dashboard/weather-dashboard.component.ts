cf28e090915976fdc436438881b605963f87fd8a
michaelc143 <mcorbishley@wisc.edu>
2023-05-29 16:59:49 -0500

    working on ngif

--- src/app/weather-dashboard/weather-dashboard.component.ts
+++ src/app/weather-dashboard/weather-dashboard.component.ts
@@ -14,4 +14,11 @@ export class WeatherDashboardComponent {
       ]
     }
     stringsArray: string[] = [];
+    ifStatement =  'this is one of the ifs';
+    flipIfStatement() {
+      if(this.ifStatement = 'this is one of the ifs')
+        this.ifStatement = 'this is the other ifs';
+      else if(this.ifStatement = 'this is the other ifs')
+        this.ifStatement = 'this is one of the ifs';
+    }
 }
